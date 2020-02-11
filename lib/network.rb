40362534c3f13c22fd5cd8462420e2e8d89ac39a
Zach Holcomb <zachholcombmusic@gmail.com>
2020-02-11 10:04:16 -0700

    Add initialize and add_show method to network class and pass tests

--- lib/network.rb
+++ lib/network.rb
@@ -1,1 +1,12 @@
+class Network
+  attr_reader :name, :shows
+  def initialize(name)
+    @name = name
+    @shows = []
+  end
 
+  def add_show(show)
+    @shows << show
+  end
+
+end
---
+++ test/network_test.rb
@@ -0,0 +1,54 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/character'
+require './lib/show'
+require './lib/network'
+
+class NetworkTest < Minitest::Test
+  def setup
+    @nbc = Network.new("NBC")
+    @michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    @knight_rider = Show.new("Knight Rider", "Glen Larson", [@michael_knight, @kitt])
+    @leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    @ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    @parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [@leslie_knope, @ron_swanson])
+
+  end
+
+  def test_it_exists_and_has_attributes
+    assert_instance_of Network, @nbc
+    assert_equal "NBC", @nbc.name
+  end
+
+  def test_it_starts_with_no_shows
+    assert_equal [], @nbc.shows
+  end
+
+  def test_it_can_add_shows
+    @nbc.add_show(@knight_rider)
+    @nbc.add_show(@parks_and_rec)
+    assert_equal [@knight_rider, @parks_and_rec], @nbc.shows
+  end
+
+end
+
+
+
+
+#
+# pry(main)>
+#
+# pry(main)>
+#
+# pry(main)> nbc.shows
+# # => [#<Show:0x00007fe5f8398970...>, #<Show:0x00007fe5f88b0a20...>]
+#
+# pry(main)> nbc.main_characters
+# # => [#<Character:0x00007f98a4ba8dc8...>]
+#
+# pry(main)> nbc.actors_by_show
+# # => {
+#       #<Show:0x00007fe5f8398970...> => ["David Hasselhoff", "William Daniels"],
+#       #<Show:0x00007fe5f88b0a20...> => ["Amy Poehler", "Nick Offerman"]
+# #    }
