68a7a2a07c1644caaa2987dab5b8b3d11749e065
Zach Holcomb <zachholcombmusic@gmail.com>
2020-02-11 09:32:14 -0700

    Setup character class, pass all initial tests

--- lib/character.rb
+++ lib/character.rb
@@ -1,1 +1,11 @@
 
+class Character
+  attr_reader :name, :actor, :salary
+
+  def initialize(info)
+    @info = info
+    @name = info[:name]
+    @actor = info[:actor]
+    @salary = info[:salary]
+  end
+end
---
+++ test/character_test.rb
@@ -0,0 +1,27 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/character'
+
+class CharacterTest < Minitest::Test
+  def setup
+    @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+  end
+
+  def test_it_exists_and_has_attributes
+    assert_instance_of Character, @kitt
+    assert_equal "KITT", @kitt.name
+    assert_equal "William Daniels", @kitt.actor
+    assert_equal 1000000, @kitt.salary
+  end
+end
+
+
+
+# pry(main)> kitt.name
+# # => "KITT"
+#
+# pry(main)> kitt.actor
+# # => "William Daniels"
+#
+# pry(main)> kitt.salary
+# # => 1000000
